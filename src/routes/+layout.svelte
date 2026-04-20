cd5c5386fe63bdc6e9ad9e0fa5bb9b9e6e5007eb
Luiz <luizcarlos1405@gmail.com>
2026-04-20 11:10:03 -0400

    max md seems better

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -55,7 +55,7 @@
   <title>Faz</title>
 </svelte:head>
 
-<div id="layout" class="flex flex-col h-full max-w-lg mx-auto relative shadow-lg bg-base-100">
+<div id="layout" class="flex flex-col h-full max-w-md mx-auto relative shadow-lg bg-base-100">
   <TopBar />
 
   <main class="flex-1 overflow-y-auto pt-14 pb-20">
